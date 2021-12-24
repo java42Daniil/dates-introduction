88d4b1f2e596ce1f072ee88364821e596c0d7bff
Daniil Berezin <daniil.berezin98@gmail.com>
2021-12-24 15:25:52 +0200

    hw23 Berezin Daniil

---
+++ dates-introduction/src/telran/util/NextFriday13Adjuster.java
@@ -0,0 +1,29 @@
+package telran.util;
+
+import java.time.DayOfWeek;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class NextFriday13Adjuster implements TemporalAdjuster {
+
+	private static final DayOfWeek FRIDAY = DayOfWeek.FRIDAY;
+	private static final int DAY = 13;
+	@Override
+	public Temporal adjustInto(Temporal temporal) {
+		temporal = setTemporal(temporal);
+		while (temporal.get(ChronoField.DAY_OF_MONTH) != DAY) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS).
+					with(TemporalAdjusters.dayOfWeekInMonth(2, FRIDAY));
+		}
+		return temporal;
+	}
+	private Temporal setTemporal(Temporal temporal) {
+		if (temporal.get(ChronoField.DAY_OF_MONTH) >= DAY) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal.with(TemporalAdjusters.dayOfWeekInMonth(2, FRIDAY));
+	}
+}
